f912cecd74716f3284be6a2fc62a650d86c83d34
Shikha <72983644+Shikha-1@users.noreply.github.com>
2023-10-27 15:51:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ function main(ctime) {
     gameEngine();
 }
 
-function isCollide(snake) {
-    //if snake bump into itself
-    for (let i = 0; i < snakeArr.length; i++) {
-        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
-            return true;
-        }
-    }
-        if (snake[0].x >= 18 || snake[0].x <= 0 && snake[0].y >= 18 || snake[0].y <= 0) {
-            return true;
-        }
-}
+// function isCollide(snake) {
+//     //if snake bump into itself
+//     for (let i = 0; i < snakeArr.length; i++) {
+//         if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+//             return true;
+//         }
+//     }
+//         if (snake[0].x >= 18 || snake[0].x <= 0 && snake[0].y >= 18 || snake[0].y <= 0) {
+//             return true;
+//         }
+// }
 
 function isCollide(snake) {
   // If you bump into yourself
